124be311e63126b4c808bd05f9d2b9a6974a7f52
Riley <username@computer-xd.riley-uwu.onthewifi.com>
2021-01-28 19:45:26 +1100

    tweaked audittest again..

--- auditTest/auditTest.py
+++ auditTest/auditTest.py
@@ -15,11 +15,10 @@ class AuditTest(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.guild)
     @commands.bot_has_permissions(embed_links=True)
-    async def listActions(ctx: commands.Context):
+    async def listActions(self, ctx: commands.Context):
         embed = discord.Embed(colour=discord.Colour.green())
-        action = discord.AuditLogAction
         i = 1
         async for entry in discord.Guild.audit_logs(user=None, limit=5):
             formatted_entry = '{0.user} did {0.action} to {0.target}'.format(entry)
             embed.add_field(name=str(i), value=formatted_entry)
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
